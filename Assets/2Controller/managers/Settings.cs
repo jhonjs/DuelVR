76695a0082346eff6b44744c672586bbe29dc856
AlexanderJV <73327911+jhonjs@users.noreply.github.com>
2022-11-26 12:37:11 -0500

    separate dependencies into managers

--- Assets/2Controller/managers/Settings.cs
+++ Assets/2Controller/managers/Settings.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Settings : GameManager
+public class Settings : MonoBehaviour
 {
     //[Range(0, 1)] public float volume;
-     public static Settings settings;
+    public MenuManger menuManger;
+    public static Settings settings;
+    public AudioSource currentSound;
 
     private void Awake()
     {
         if (settings != null) Destroy(this);
         else DontDestroyOnLoad(gameObject);
+        menuManger = FindObjectOfType<MenuManger>();
     }
     private void Start()
     {
-        sound.Play();
+        currentSound = menuManger.sound;
+        currentSound.Play();
     }
     public void volume(float volume)
     {
-        sound.volume = volume;
+        currentSound.volume = volume;
     }
 
-    override public void Exit()
-    {
-        sound.Stop();
-    }
 }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Videojuego en realidad aumentada para capacitacion docente
 * https://www.atlassian.com/es/git/tutorials/using-branches/git-checkout
 * https://www.freepik.es/vector-gratis/
 * https://pixabay.com/music/search/genre/video%20games/
+* https://www.mixamo.com/
