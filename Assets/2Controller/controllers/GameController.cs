3fa69970f9df3379645e6722aa4a2f68e8fc371a
AlexanderJV <73327911+jhonjs@users.noreply.github.com>
2022-12-04 10:45:46 -0500

    Add models rv to duel module

---
+++ Assets/2Controller/controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform spawnPoint;
+    public bool isCreate;
+
+    private void OnMouseDown()
+    {
+        if (isCreate) return;
+        else {
+            Instantiate(player, spawnPoint.position, spawnPoint.rotation, spawnPoint);
+            isCreate = true;
+        }
+    }
+}
