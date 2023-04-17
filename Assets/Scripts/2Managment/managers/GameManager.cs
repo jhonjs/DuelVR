efc4f03baa16276fc0b384150966fdb5f923fc84
AlexanderJV <73327911+jhonjs@users.noreply.github.com>
2023-04-16 23:11:06 -0500

    add score and default mision to word mode

--- Assets/Scripts/2Managment/managers/GameManager.cs
+++ Assets/Scripts/2Managment/managers/GameManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using TMPro;
+
 public class GameManager : MonoBehaviour
 {
+    private string currentScene = "";
     [SerializeField] GameObject[] panelsHide;
     public enum gameState { menu, play, pause }
     public gameState state;
+
     public AudioSource sound;
     Player player = new();
 
     public DataCard[] _skills;
+    
+    [SerializeField] private TextMeshProUGUI txtAmountCards;
+    [SerializeField] private GameObject panelWinner;
+    [SerializeField] private GameObject folderCards;
+    private int amountCards = 0;
+    public int cardsPublished = 11;
+
+    [SerializeField] private TextMeshProUGUI txtScore;
+    [SerializeField] private TextMeshProUGUI txtTimer;
+    private float score;
+    private float time = 0;
+    private bool endGame;
+
 
     private void Start()
     {
@@ -20,11 +37,31 @@ public class GameManager : MonoBehaviour
         {
             showPanel.SetActive(false);
         }
+        if(txtAmountCards != null) txtAmountCards.text = $"{amountCards}/{cardsPublished}";
+        amountCards = 0;
+        currentScene = SceneManager.GetActiveScene().name;
     }
+
+    private void FixedUpdate()
+    {
+        if (currentScene == "World")
+        {
+            if(!endGame) time = Time.time;
+            txtTimer.text = ((int) time ).ToString();
+
+            if (time == 300)
+            {
+                score = ((100 / amountCards) * 4) - ((int)time / 4);
+                txtScore.text = $"{(int) score}";
+                panelWinner.SetActive(true);
+            }
+        }
+    }
+
     virtual public void ChangeScene(string scene)
     {
+        time = 0;
         StartCoroutine("ChangeSceneTimeOut", scene);
-
     }
     virtual public void PlaySound(AudioSource sound)
     {
@@ -37,7 +74,7 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator ChangeSceneTimeOut(string scene)
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(scene);
     }
 
@@ -57,12 +94,26 @@ public class GameManager : MonoBehaviour
     {
         var playerMaster = JObject.Parse(PlayerPrefs.GetString("player"));
 
+        amountCards += 1;
+        txtAmountCards.text = $"{amountCards}/{cardsPublished}";
+
+        if (amountCards == cardsPublished)
+        {
+            score = 400 - ((int) time / 4);
+            if (time < 30) score = 400;
+
+            txtScore.text = $"{(int) score}";
+            endGame = true;
+            panelWinner.SetActive(true);
+            folderCards.SetActive(false);
+            return;
+        }
+
         player.id = (int) playerMaster.SelectToken("id");
         player._name = playerMaster.SelectToken("_name").ToString();
         player._age = (int) playerMaster.SelectToken("_age");
 
         if(!player.skills.Contains(skill)) player.skills.Add(skill);
-
         player.amountSkills = player.skills.Count;
 
         PlayerPrefs.SetString("player", JsonUtility.ToJson(player)); 
--- Assets/Scripts/3View/controllers/SkillController.cs
+++ Assets/Scripts/3View/controllers/SkillController.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SkillController : MonoBehaviour
 {
     public GameManager gameManager;
+    [SerializeField] private TextMeshProUGUI amount;
 
     private void OnMouseDown()
     {
         gameManager.addPlayerSkill(name);
+        Destroy(gameObject, 2);
     }
 }
